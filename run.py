1debb5099b283d3413539e912b9e57d2bebd5d0f
unixshaman <unixshaman@gmail.com>
2020-09-19 23:52:41 +0300

    Install pyinstaller Command for compiling releases pyinstaller --name CerediraTessRelease --add-data "www;www" --add-data "scripts;scripts" --add-data "resources;resources" run.py

--- run.py
+++ run.py
@@ -1,6 +1,9 @@
+import multiprocessing
+
 from cerediratess import create_app
 
 app = create_app()
 
 if __name__ == "__main__":
+    multiprocessing.freeze_support()
     app.run(host="0.0.0.0", port=7801, debug=True)
