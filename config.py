555307884f5a46f0cb656e4ae3e2bc823d0ba793
unixshaman <unixshaman@gmail.com>
2020-11-16 15:14:59 +0300

    Move /admin/static to /static Add environment variables

--- config.py
+++ config.py
@@ -1,16 +1,21 @@
 import os
 
-BASEDIR = os.path.abspath(os.path.dirname(__file__))
+BASEDIR = os.environ.get('CT_BASEDIR') or os.path.abspath(os.path.dirname(__file__))
 
 
 class Config(object):
-    DEBUG = True
-    # TESTING = False
-    # CSRF_ENABLED = True
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'mysecret'
-    SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL') or f'sqlite:///{BASEDIR}\\CerediraTess.db'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    CSRF_ENABLED = True
+    DEBUG = bool(os.environ.get('CT_DEBUG')) if os.environ.get('CT_DEBUG') else False
     FLASK_ADMIN_SWATCH = 'flatly'
+    SECRET_KEY = os.environ.get('CT_SECRET_KEY') or 'mysecret'
+    SERVER_CERT = os.environ.get('CT_SERVER_HTTPS_CERT') or None
+    SERVER_HOST = os.environ.get('CT_SERVER_HOST') or '0.0.0.0'
+    SERVER_HTTPS = bool(os.environ.get('CT_SERVER_HTTPS')) or False
+    SERVER_KEY = os.environ.get('CT_SERVER_HTTPS_KEY') or None
+    SERVER_PORT = int(os.environ.get('CT_SERVER_PORT')) if os.environ.get('CT_SERVER_PORT') else 7801
+    SQLALCHEMY_DATABASE_URI = os.environ.get('CT_DATABASE_URL') or f'sqlite:///{BASEDIR}\\CerediraTess.db'
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    TESTING = False
 
 
 class ProductionConfig(Config):
