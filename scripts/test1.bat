e8b1f45c930c528faba760944cc1d49c5160e463
unixshaman <unixshaman@gmail.com>
2021-03-09 00:48:02 +0300

    Remove site-packages libs files. Add command for building with files from flask-admin site-package.

--- scripts/test1.bat
+++ scripts/test1.bat
@@ -1,9 +1,27 @@
 @echo off
 chcp 65001
 
+rem script description: Скрипт для установки клиента Git
+
+
+rem param description: Название архива, содержащего пакет Git для установки
+rem param example: Git-2.23.2.7z
+set portableGitArchive=%~1
+
+
+rem param description: Название архива, содержащего пакет Git для установки
+rem param example: Git-2.23.2.7z
+set portableGitArchive=%~2
+
+
+rem param description: Токен для авторизации
+rem param example: s.er43jwejD)F3jE23jeFjew20rgr43
+set token=%~3
+
+
+echo %portableGitArchive%
+echo %portableGitArchive%
+echo %token%
 echo asdf
-echo asdf
-echo asdf
-echo %1
 
 echo привет, как дела. добрый
