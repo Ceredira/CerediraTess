d851de84b6fa2c1b73c136820b3ee2c77ffe0488
Dzhamalov Gazimagomed <unixshaman@gmail.com>
2022-05-03 00:13:05 +0300

    Update commons.py

--- ceredira_tess/commons.py
+++ ceredira_tess/commons.py
@@ -6,3 +6,17 @@ from ceredira_tess.agents_locker import AgentsLocker
 
 app = Blueprint('app', __name__, template_folder='templates')
 agents_locker = AgentsLocker()
+
+
+def generate_random_string(length):
+    import string
+    import secrets
+    alphabet = string.ascii_letters + string.digits
+    password = ''
+    while True:
+        password = ''.join(secrets.choice(alphabet) for i in range(length))
+        if (any(c.islower() for c in password)
+                and any(c.isupper() for c in password)
+                and sum(c.isdigit() for c in password) >= 3):
+            break
+    return password
