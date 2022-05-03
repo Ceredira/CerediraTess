3dd7fd8b34ddba4a2e0522ab0b9d9a62a33c17ce
Dzhamalov Gazimagomed <unixshaman@gmail.com>
2022-05-03 15:06:16 +0300

    Feature/32/add agents on script editing and viewing (#33)

--- ceredira_tess/models/script.py
+++ ceredira_tess/models/script.py
@@ -1,10 +1,12 @@
 from ceredira_tess.db import db
+from ceredira_tess.models import relationships
 
 
 class Script(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(2048), nullable=False, unique=True)
     description = db.Column(db.Text)
+    agents = db.relationship("Agent", secondary=relationships.agents_scripts, back_populates='scripts')
 
     def __repr__(self):
         return f'{self.name}'
