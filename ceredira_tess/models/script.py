7098033d32cbb88a8b593538fab97ab299665a3d
Dzhamalov Gazimagomed <unixshaman@gmail.com>
2022-05-03 15:02:14 +0300

    Update script.py

--- ceredira_tess/models/script.py
+++ ceredira_tess/models/script.py
@@ -5,6 +5,7 @@ class Script(db.Model):
     id = db.Column(db.Integer, primary_key=True, autoincrement=True)
     name = db.Column(db.String(2048), nullable=False, unique=True)
     description = db.Column(db.Text)
+    agents = db.relationship("Agent", secondary=relationships.agents_scripts, back_populates='scripts')
 
     def __repr__(self):
         return f'{self.name}'
